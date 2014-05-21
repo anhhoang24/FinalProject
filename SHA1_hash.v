3788a012f44c6c4abf3f7b60c7281144a5b1e486
Andrei Thompson <andreiwthomps@gmail.com>
2014-05-21 16:05:50 -0700

    Started setting up the reading functionality.

--- SHA1_hash.v
+++ SHA1_hash.v
@@ -12,7 +12,13 @@ module SHA1_hash (
         port_A_addr,
         port_A_we
 	);
-
+	
+//define endian switch function:
+function [31:0] changeEndian;
+		input [31:0] value;
+		changeEndian = { value[7:0], value[15:8], value[23:16], value[31:24]};
+endfunction
+		
 input	clk;
 input	nreset; 
 // Initializes the SHA1_hash module
@@ -53,11 +59,73 @@ parameter READ = 2'b01;
 parameter WRITE = 2'b10;
 parameter COMPUTE = 2'b11;
 
+reg [159:0] 	read_hash_data;
+reg [15:0]		read_addr;
+reg [2:0]		words_read;
+reg [1:0]		state;
+reg				wen;
+
+wire [159:0]	read_hash_data_n;
+wire [15:0]		read_addr_n;
+wire [2:0]		words_read_n;
+
+
+
+
+/////////ASSIGNMENTS//////////////
+
+//READ
+assign words_read_n = words_read + 1;
+
+//READ/WRITE
+assign port_A_addr = read_addr;
+assign port_A_clk = clk;
+assign read_hash_data_n = {read_hash_data[127:0],  changeEndian(port_A_data_out)}; //shift in data
+
+//WRITE
+assign port_A_we = wen;
+
+
 
 always@(posedge clk or negedge nreset)
 begin
 	if(!nreset) begin
 		//reset all registers
+		wen <= 1'b0;
+		state <=	IDLE;
+		words_read <= 3'b0;
+		read_hash_data <= 160'b0;
+	end
+	else begin
+		case(state)
+			
+			IDLE: begin
+				if(start_hash) begin
+					read_addr <= message_addr[15:0];
+					state <= READ;
+					words_read <= 3'b0;
+					read_hash_data <= 160'b0;
+				end
+			end
+			
+			READ: begin
+				read_addr <= read_addr_n;
+				//TODO: I think I am reading the data wrong. should be 512 instead of 160
+				read_hash_data <= read_hash_data_n;
+				words_read <= 3'b0;
+			end
+			
+			WRITE: begin
+			
+			
+			end
+			
+			COMPUTE: begin
+			
+			
+			end
+		
+		endcase
 	end
 
 
