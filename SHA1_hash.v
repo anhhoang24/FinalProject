f20e13be04899a05aea5c93d075261c5b0965f03
Andrei Thompson <andreiwthomps@gmail.com>
2014-06-04 18:05:33 -0700

    Added the untested SHA1 algorithm.

--- SHA1_hash.v
+++ SHA1_hash.v
@@ -61,15 +61,14 @@ parameter COMPUTE = 2'b11;
 
 integer i;
 
-reg [511:0] 	read_hash_data;
-reg [31:0]		MD[0:4], current_length, byte_n;
+reg [31:0]		runMD[0:4], currMD[0:4], current_length, byte_n, W_t, read_hash_data[15:0], K_t, F_b_c_d, T;
 reg [15:0]		read_addr;
+reg [6:0]		count_t;
 reg [3:0]		words_read;
 reg [1:0]		state;
 reg				wen, init_read;
 
-wire [511:0]	read_hash_data_n;
-wire [31:0]		byte_read_n, total_length;
+wire [31:0]		byte_read_n, total_length, A, B, C, D, E;
 wire [15:0]		read_addr_n;
 wire [9:0]		zero_pad_length;
 wire [3:0]		words_read_n;
@@ -85,19 +84,28 @@ assign zero_pad_length = 512 - (((8 * message_size) + 65) % 512);
 assign total_length = (message_size * 8) + 1 + zero_pad_length + 64;
 
 //READ
-assign words_read_n = words_read + 1;
+assign words_read_n = (words_read + 1) % 16;
 assign read_addr_n = read_addr + 4; //increment the read address
 
 //READ/WRITE
 assign port_A_addr = read_addr;
 assign port_A_clk = clk;
 assign byte_read_n = changeEndian(port_A_data_out);
-assign read_hash_data_n = {read_hash_data[479:0],  byte_n}; //shift in data
 assign done = (current_length == total_length) && (state == IDLE);
 
 //WRITE
 assign port_A_we = wen;
 
+//COMPUTE:
+assign A = currMD[0];
+assign B = currMD[1];
+assign C = currMD[2];
+assign D = currMD[3];
+assign E = currMD[4];
+
+//OUT
+assign hash = {changeEndian(runMD[4]),changeEndian(runMD[3]),
+						changeEndian(runMD[2]),changeEndian(runMD[1]),changeEndian(runMD[0])};
 
 
 always@(*)
@@ -123,6 +131,38 @@ begin
 	else begin
 		byte_n <= byte_read_n;
 	end
+	
+	
+	//compute current W_t:
+	if(count_t < 16) begin
+		W_t <= read_hash_data[count_t];
+	end
+	else begin
+		W_t <= (read_hash_data[count_t-3] ^ 
+					read_hash_data[count_t-8] ^ read_hash_data[count_t-14] ^ 
+					read_hash_data[count_t-16] ^ read_hash_data[count_t-3]) <<< 1;
+	end
+	
+	//compute current K_t and F_b_c_d
+	if(count_t < 20) begin
+		K_t <= 32'h5a827999;
+		F_b_c_d <= (B & C) | ((~B) & D);
+	end
+	else if(count_t < 40) begin
+		K_t <= 32'h6ed9eba1;
+		F_b_c_d <= B ^ C ^ D;
+	end
+	else if(count_t < 60) begin
+		K_t <= 32'h8f1bbcdc;
+		F_b_c_d <= (B & C) | (B & D) | (C & D);
+	end
+	else begin
+		K_t <= 32'hca62c1d6;
+		F_b_c_d <= B ^ C ^ D;
+	end
+	
+	//compute value of T
+	T <= (A <<< 5) + F_b_c_d + W_t + K_t + E;
 end
 
 
@@ -136,11 +176,16 @@ begin
 		wen <= 1'b0;
 		state <=	IDLE;
 		words_read <= 3'b0;
-		read_hash_data <= 512'b0;
 		current_length <= 32'b0;
+		count_t <= 7'b0;
 		for(i = 0; i < 5; i = 1 + i) begin
-			MD[i] <= 32'b0;
+			currMD[i] <= 32'b0;
+			runMD[i] <= 32'b0;
 		end
+		for(i = 0; i < 16; i = 1 + i) begin
+			read_hash_data[i] <= 32'b0;
+		end
+		
 		init_read <= 1'b0;
 	end
 	else begin
@@ -151,15 +196,17 @@ begin
 					read_addr <= message_addr[15:0];
 					state <= READ;
 					words_read <= 3'b0;
-					read_hash_data <= 512'b0;
+					for(i = 0; i < 16; i = 1 + i) begin
+						read_hash_data[i] <= 32'b0;
+					end
 					init_read <= 1'b1;
 					
 					//initialize to M values:
-					MD[0] <= 32'h67452301;
-					MD[1] <= 32'hefcdab89;
-					MD[2] <= 32'h98badcfe;
-					MD[3] <= 32'h10325476;
-					MD[4] <= 32'hc3d2e1f0;
+					runMD[0] <= 32'h67452301;
+					runMD[1] <= 32'hefcdab89;
+					runMD[2] <= 32'h98badcfe;
+					runMD[3] <= 32'h10325476;
+					runMD[4] <= 32'hc3d2e1f0;
 				end
 				if(wen) begin
 					wen <= 1'b0;
@@ -169,9 +216,22 @@ begin
 			READ: begin
 				read_addr <= read_addr_n;
 				if(!init_read) begin
-					read_hash_data <= read_hash_data_n;
+					
+					//shift data:
+					read_hash_data[15] <= byte_n;
+					for(i = 14; i >= 0; i = i - 1) begin
+						read_hash_data[i] = read_hash_data[i+1];
+					end
 					words_read <= words_read_n;
-					state <= (words_read_n) ? READ : COMPUTE; //check if we have filled the buffer
+					
+					//POTENTIAL HAZARD: this may be either == 15 or == 16
+					if(words_read_n == 15) begin
+						state <= COMPUTE; //check if we have filled the buffer
+						for(i = 0; i < 5; i = 1 + i) begin
+							currMD[i] <= runMD[i];
+						end
+					end
+					
 					current_length <= current_length + 4; // keep running count of current length
 				end
 				else init_read <= 1'b0;
@@ -183,7 +243,23 @@ begin
 			end
 			
 			COMPUTE: begin
-				state <= (current_length == total_length)? IDLE : READ; //test
+				count_t <= (1 + count_t) % 80; //increment count_t
+				if(count_t < 79) begin
+					//Perform Algorithm:
+					currMD[0] <= T;
+					currMD[1] <= A;
+					currMD[2] <= (B <<< 30);
+					currMD[3] <= C;
+					currMD[4] <= D;
+				end
+				else if(count_t == 79) begin
+					state <= (current_length == total_length) ? IDLE : READ;
+					runMD[0] <= runMD[0] + T;
+					runMD[1] <= runMD[1] + A;
+					runMD[2] <= runMD[2] + (B <<< 30);
+					runMD[3] <= runMD[3] + C;
+					runMD[4] <= runMD[4] + D;
+				end
 			end
 		
 		endcase
